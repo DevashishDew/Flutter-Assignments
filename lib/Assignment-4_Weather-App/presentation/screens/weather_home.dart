a59205601dac9d4a7f37d8bd9af2766529b9e710
DevashishDew <kbdevashish@dewsolutions.in>
2023-07-05 16:43:26 +0530

    assignment-4(weather app) : code refactor

--- lib/Assignment-4_Weather-App/presentation/screens/weather_home.dart
+++ lib/Assignment-4_Weather-App/presentation/screens/weather_home.dart
@@ -97,7 +97,24 @@ class _WeatherHomePageState extends State<WeatherHomePage> {
                         ),
                       ],
                     ),
-                    BlocListener<GeolocationBloc, GeolocationState>(
+                    BlocConsumer<GeolocationBloc, GeolocationState>(
+                      builder: (context, state) {
+                        if (state is GeolocationLoaded) {
+                          return mainContent();
+                        } else if (state is GeolocationPermissionDenied ||
+                            state is GeolocationPermissionForever) {
+                          return const Text(
+                            'Please enable location '
+                            'permission to access weather data!',
+                            style: TextStyle(
+                                fontWeight: FontWeight.w500, fontSize: 18),
+                          );
+                        } else if (state is GeolocationLoading) {
+                          return const ProgressIndicatorWidget();
+                        } else {
+                          return const Text('Something went wrong!');
+                        }
+                      },
                       listener: (context, state) {
                         if (state is GeolocationLoaded) {
                           BlocProvider.of<CurrentWeatherBloc>(context).add(
@@ -111,71 +128,7 @@ class _WeatherHomePageState extends State<WeatherHomePage> {
                                   state.position.longitude));
                         }
                       },
-                      child: Column(
-                        children: [
-                          BlocBuilder<CurrentWeatherBloc, CurrentWeatherState>(
-                            builder: (context, state) {
-                              if (state is CurrentWeatherHasData) {
-                                return (state.currentWeatherData != null)
-                                    ? MainWeatherWidget(
-                                        currentWeatherData:
-                                            state.currentWeatherData)
-                                    : const Center(
-                                        child: Text('No data available!'),
-                                      );
-                              }
-                              if (state is CurrentWeatherError) {
-                                return Text(state.error);
-                              } else {
-                                return const ProgressIndicatorWidget();
-                              }
-                            },
-                          ),
-                          BlocBuilder<HourlyWeatherBloc, HourlyWeatherState>(
-                            builder: (context, state) {
-                              if (state is HourlyWeatherData) {
-                                return (state.hourlyForecastData != null)
-                                    ? HourlyForecast(
-                                        hourlyForecastData:
-                                            state.hourlyForecastData)
-                                    : const Center(
-                                        child: Text('No data available!'),
-                                      );
-                              }
-                              if (state is HourlyWeatherError) {
-                                return Text(state.error);
-                              } else {
-                                return const ProgressIndicatorWidget();
-                              }
-                            },
-                          ),
-                          const SizedBox(
-                            height: 16,
-                          ),
-                          BlocBuilder<DailyWeatherBloc, DailyWeatherState>(
-                            builder: (context, state) {
-                              if (state is DailyWeatherData) {
-                                return (state.dailyForecastData != null)
-                                    ? DailyForecast(
-                                        dailyForecastData:
-                                            state.dailyForecastData)
-                                    : const Center(
-                                        child: Text('No data available!'),
-                                      );
-                              }
-                              if (state is DailyWeatherError) {
-                                return Text(state.error);
-                              } else {
-                                return const ProgressIndicatorWidget();
-                              }
-                            },
-                          ),
-                          const SizedBox(
-                            height: 16,
-                          ),
-                        ],
-                      ),
-                    ),
+                    )
                   ],
                 ),
               ),
@@ -183,6 +136,68 @@ class _WeatherHomePageState extends State<WeatherHomePage> {
           ),
         ),
       ),
+    );
+  }
+
+  Column mainContent() {
+    return Column(
+      children: [
+        BlocBuilder<CurrentWeatherBloc, CurrentWeatherState>(
+          builder: (context, state) {
+            if (state is CurrentWeatherHasData) {
+              return (state.currentWeatherData != null)
+                  ? MainWeatherWidget(
+                      currentWeatherData: state.currentWeatherData)
+                  : const Center(
+                      child: Text('No data available!'),
+                    );
+            }
+            if (state is CurrentWeatherError) {
+              return Text(state.error);
+            } else {
+              return const ProgressIndicatorWidget();
+            }
+          },
+        ),
+        BlocBuilder<HourlyWeatherBloc, HourlyWeatherState>(
+          builder: (context, state) {
+            if (state is HourlyWeatherData) {
+              return (state.hourlyForecastData != null)
+                  ? HourlyForecast(hourlyForecastData: state.hourlyForecastData)
+                  : const Center(
+                      child: Text('No data available!'),
+                    );
+            }
+            if (state is HourlyWeatherError) {
+              return Text(state.error);
+            } else {
+              return const ProgressIndicatorWidget();
+            }
+          },
+        ),
+        const SizedBox(
+          height: 16,
+        ),
+        BlocBuilder<DailyWeatherBloc, DailyWeatherState>(
+          builder: (context, state) {
+            if (state is DailyWeatherData) {
+              return (state.dailyForecastData != null)
+                  ? DailyForecast(dailyForecastData: state.dailyForecastData)
+                  : const Center(
+                      child: Text('No data available!'),
+                    );
+            }
+            if (state is DailyWeatherError) {
+              return Text(state.error);
+            } else {
+              return const ProgressIndicatorWidget();
+            }
+          },
+        ),
+        const SizedBox(
+          height: 16,
+        ),
+      ],
     );
   }
 }
